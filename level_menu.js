4fc55a4347305d1f7929084ea64f8331d0b63c03
NetherForce <nederforce@gmail.com>
2020-04-19 21:42:00 +0300

    finished menu

--- level_menu.js
+++ level_menu.js
@@ -6,22 +6,49 @@ function setLevel(level_){
 
 var isAvailable = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ], br = 100;
 let time = 0;
+var pairOfButtons = document.getElementsByClassName("pairOfButton");
 var buttons = document.getElementsByClassName("button");
+var buttonLinks = document.getElementsByClassName("buttonLink");
+var unactiveButtons = document.getElementsByClassName("unactiveButton");
+var menu = document.getElementById("Menu");
 var selectedLevel = 0;
+
+function fillLevels(){
+    for(let i=1; i<br; i++){
+        if(pairOfButtons.length>0 && menu!=null){
+            let newRow = pairOfButtons[0].cloneNode(true);
+            menu.appendChild(newRow);
+
+            pairOfButtons = document.getElementsByClassName("pairOfButton");
+            buttons = document.getElementsByClassName("button");
+            buttonLinks = document.getElementsByClassName("buttonLink");
+            unactiveButtons = document.getElementsByClassName("unactiveButton");
+
+            buttons[i].innerHTML = i+1;
+            unactiveButtons[i].innerHTML = i+1;
+            if((i+1)%10==0){
+                buttons[i].style.backgroundColor = "#ffcc00";
+                buttons[i].style.borderColor = "#cca300";       
+            }
+            if(isAvailable[i]==0){
+                buttonLinks[i].style.display = "none";
+                unactiveButtons[i].style.display = "inline";
+            }else{
+                buttonLinks[i].style.display = "inline";
+                unactiveButtons[i].style.display = "none";
+            }
+        }
+    }
+}
 
 function setState(){
     for(let i=0; i<br; i++){
         if(isAvailable[i]==0){
-            buttons[i].style.backgroundColor = "#999999";
-            buttons[i].style.borderColor = "#737373";
+            buttonLinks[i].style.display = "none";
+            unactiveButtons[i].style.display = "inline";
         }else{
-            if(i>1 && (i+1)%10==0){
-                buttons[i].style.backgroundColor = "#ffcc00";
-                buttons[i].style.borderColor = "#cca300";
-            }else{
-                buttons[i].style.backgroundColor = "#3399ff";
-                buttons[i].style.borderColor = "#0066ff";
-            }
+            buttonLinks[i].style.display = "inline";
+            unactiveButtons[i].style.display = "none";
         }
     }
 }
@@ -29,15 +56,18 @@ function setState(){
 
 
 function update() {
-    if(buttons.length==100){
+    if(pairOfButtons.length>0 && menu!=null){
         if(time%20==0){
             if(time==0){
-               buttons[99].style.fontSize = "22px";
+                fillLevels();
+                buttons[99].style.fontSize = "22px";
             }
             setState();
         }
         time++;
         setTimeout(update, 10);
+    }else{
+        menu = document.getElementById("Menu");
     }
 }
 function startUpdate(){
